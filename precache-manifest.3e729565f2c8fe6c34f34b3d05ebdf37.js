060f6b2bf0200adf44e8cd3a4d57e466122c04b5
Abdelfattah <abdelfattah@mindee.co>
2020-07-20 12:07:43 +0200

    Updates

---
+++ precache-manifest.3e729565f2c8fe6c34f34b3d05ebdf37.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c731d99b1b5c4e6128908378e295d5f7",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.94b42598e0bad8300a43.bundle.js"
+  },
+  {
+    "url": "runtime~main.94b42598e0bad8300a43.bundle.js"
+  },
+  {
+    "revision": "b24719497d328c2ce152",
+    "url": "static/css/vendors~main.76cf7f4f.chunk.css"
+  },
+  {
+    "revision": "6362c410031f0b1afd5d9f37155bbe57",
+    "url": "static/media/default-placeholder.6362c410.png"
+  },
+  {
+    "revision": "76d2e43dc9ebea5569bb3ce1722b21b3",
+    "url": "static/media/download-placeholder.76d2e43d.png"
+  },
+  {
+    "revision": "27707f2713203e24a6e7ebaeef854c9c",
+    "url": "static/media/dummy-pdf.27707f27.pdf"
+  },
+  {
+    "revision": "35be770fc34068481b29c7b64b8c77b7",
+    "url": "static/media/fullscreen.35be770f.svg"
+  },
+  {
+    "revision": "30c0e6ac37f83686b73fb13bc157b601",
+    "url": "static/media/mindee-logo.30c0e6ac.gif"
+  },
+  {
+    "revision": "33439d0e315a90302ebe742cf3b8e455",
+    "url": "static/media/pdf.33439d0e.svg"
+  },
+  {
+    "revision": "b382169b2a0b71aeca381929407dff8e",
+    "url": "static/media/uploader.b382169b.svg"
+  },
+  {
+    "url": "vendors~main.94b42598e0bad8300a43.bundle.js"
+  },
+  {
+    "url": "vendors~main.94b42598e0bad8300a43.bundle.js.LICENSE.txt"
+  }
+]);
